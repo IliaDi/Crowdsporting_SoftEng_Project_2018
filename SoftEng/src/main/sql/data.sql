973f45a0b8a4f3fb6e9e81caa024ab57f1d2b779
Ilia D <ilia.dioleti6@gmail.com>
2019-01-10 15:33:59 +0200

    updated database

--- SoftEng/src/main/sql/data.sql
+++ SoftEng/src/main/sql/data.sql
@@ -1,50 +1,68 @@
 CREATE DATABASE `crowdsporting`;
 
-CREATE TABLE `crowdsporting`.`provider` (
-`name` VARCHAR(100) NOT NULL UNIQUE,
-`street`  VARCHAR(100) NOT NULL,
-`number` INT NOT NULL,
-`postal_code` INT NOT NULL ,
-`city` VARCHAR(100) NOT NULL ,
-`category` ENUM(
-    'gym',
-    'dance',
-    'sports team',
-    'yoga',
-    'pilates',
-    'other'
-  ),
-`mail` VARCHAR(320) ,
-`phone` VARCHAR(15) NOT NULL,
-`website` VARCHAR(320) ,
-PRIMARY KEY (`name`)
+DROP TABLE IF EXISTS `crowdsporting`.`shop`;
+CREATE TABLE `crowdsporting`.`shop`
+(
+  `id`          int(11)      NOT NULL AUTO_INCREMENT,
+  `name`        VARCHAR(255) NOT NULL UNIQUE,
+  `street`      VARCHAR(100) NOT NULL,
+  `number`      INT          NOT NULL,
+  `postal_code` INT          NOT NULL,
+  `city`        VARCHAR(100) NOT NULL,
+  `lng`         DOUBLE,
+  `lat`         DOUBLE,
+  `mail`        VARCHAR(320),
+  `phone`       VARCHAR(15)  NOT NULL,
+  `website`     VARCHAR(320),
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+
+DROP TABLE IF EXISTS `crowdsporting`.`product`;
+CREATE TABLE `crowdsporting`.`product`
+(
+  `id`          int(11)      NOT NULL AUTO_INCREMENT,
+  `name`        varchar(255) NOT NULL,
+  `description` mediumtext,
+  `category`    varchar(128) NOT NULL,
+  `withdrawn`   bool         NOT NULL DEFAULT false,
+  `likes`       int(11),
+  `prov_id`     int(11)      NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`prov_id`) REFERENCES `crowdsporting`.`shop` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+DROP TABLE IF EXISTS `crowdsporting`.`product_tags`;
+CREATE TABLE `crowdsporting`.`product_tags`
+(
+  `pid` int(11)      NOT NULL DEFAULT '0',
+  `tag` varchar(255) NOT NULL,
+  PRIMARY KEY (`pid`, `tag`),
+  FOREIGN KEY (`pid`) REFERENCES `crowdsporting`.`product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+DROP TABLE IF EXISTS `crowdsporting`.`shop_tags`;
+CREATE TABLE `crowdsporting`.`shop_tags`
+(
+  `sid` int(11)      NOT NULL DEFAULT '0',
+  `tag` varchar(255) NOT NULL,
+  PRIMARY KEY (`sid`, `tag`),
+  FOREIGN KEY (`sid`) REFERENCES `crowdsporting`.`shop` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+DROP TABLE IF EXISTS `crowdsporting`.`user`;
+CREATE TABLE `crowdsporting`.`users`
+(
+  `username` VARCHAR(100) NOT NULL UNIQUE,
+  `password` VARCHAR(12)  NOT NULL,
+  `points`   INT          NOT NULL DEFAULT 0,
+  `mail`     VARCHAR(320) NOT NULL,
+  PRIMARY KEY (`mail`)
 ) ENGINE = InnoDB;
 
-CREATE TABLE `crowdsporting`.`activity` (
-`activity_id` INT NOT NULL AUTO_INCREMENT ,
-`name` VARCHAR(100) NOT NULL,
-`description` VARCHAR(500),
-`prov_name` VARCHAR(100) NOT NULL,
-`likes` INT ,
-PRIMARY KEY (`activity_id`),
-FOREIGN KEY (`prov_name`) REFERENCES provider(`name`)
-ON DELETE CASCADE
-ON UPDATE CASCADE
-) ENGINE = InnoDB;
-
-CREATE TABLE `crowdsporting`.`users` (
-`username` VARCHAR(100) NOT NULL UNIQUE ,
-`password` VARCHAR(12) NOT NULL,
-`points` INT NOT NULL DEFAULT 0,
-`mail` VARCHAR(320) NOT NULL ,
-PRIMARY KEY (`mail`)
-) ENGINE = InnoDB;
-
-INSERT INTO crowdsporting.provider (name, street, number,  postal_code, city, category, mail , phone, website)
-VALUES ('Get fit', 'Iera odos', 199, 12245, 'Athens', 'gym', 'getfitgym@gmail.com' , '2105999321' , null);
-
-INSERT INTO crowdsporting.activity ( name, description, prov_name ,likes)
-VALUES ( 'yoga', 'συνδρομή ενός μήνα, 4 μαθήματα την εβδομάδα', 'Get fit', 0);
-
-INSERT INTO crowdsporting.users (username,password, fname, lname, points, mail)
-VALUES ('dummy', '1q2w3e', 'John', 'Doe', 0 , 'j_doe@gmail.com');
