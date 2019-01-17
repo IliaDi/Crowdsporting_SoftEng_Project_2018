fce6e3fefd331c05a617f6a4505f8dc7c5a263b0
orestis <orestischar@gmail.com>
2019-01-17 15:46:06 +0200

    The files in their directories

---
+++ SoftEng/src/main/java/data/ProductRowMapper.java
@@ -0,0 +1,23 @@
+package data;
+
+import data.model.Product;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ProductRowMapper implements RowMapper {
+
+    @Override
+    public Product mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+        long id            = rs.getLong("id");
+        String name        = rs.getString("name");
+        String description = rs.getString("description");
+        String category    = rs.getString("category");
+        boolean withdrawn  = rs.getBoolean("withdrawn");
+
+        return new Product(id, name, description, category, withdrawn);
+    }
+
+}
