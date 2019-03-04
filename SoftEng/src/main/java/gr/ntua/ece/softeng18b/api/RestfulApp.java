6e9d0cbddbf117dfb1e1a9967f51456d3b34bcae
georgios-ts <45130028+georgios-ts@users.noreply.github.com>
2019-03-04 03:23:19 +0200

    Update RestfulApp.java

--- SoftEng/src/main/java/gr/ntua/ece/softeng18b/api/RestfulApp.java
+++ SoftEng/src/main/java/gr/ntua/ece/softeng18b/api/RestfulApp.java
@@ -13,19 +13,32 @@ public class RestfulApp extends Application {
 
         //GET, POST
         router.attach("/products", ProductsResource.class);
+       
+        //GET, POST
+        router.attach("/shops", ShopsResource.class);
 
         //GET, POST
         router.attach("/prices", PriceResource.class);
 
         //GET, DELETE
         router.attach("/products/{id}", ProductResource.class);
-
-        //GET, DELETE
+        
+         //GET, DELETE
         router.attach("/shops/{id}", ShopResource.class);
+        
+
+        //GET
+        router.attach("/productname/{name}", ProductNameResource.class);
+       
+        //GET
+        router.attach("/shopsname", ShopsNameResource.class);
 
 
+        //POST
+        router.attach("/signup", UserResource.class);
         router.attach("/login",  LoginResource.class);        
         router.attach("/logout", LogoutResource.class);
+        
 
         return router;
     }
